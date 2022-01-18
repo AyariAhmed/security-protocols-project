98d4d74604442f616e4a34ce2b392164a2fc890f
AyariAhmed <ayari17@hotmail.fr>
2022-01-18 23:50:36 +0100

    reafactor: reformat main menu

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from simple_term_menu import TerminalMenu
 from cryptographic_tools.coding import Encoding
 from cryptographic_tools.hashing import Hash
+from os import system
+
+
+def clear():
+    system('clear')
 
 
 def main_menu():
     while True:
-        m_choices = ['0- Encoding', '1- Hashing', '2- Quit']
+        m_choices = ['0- Encoding', '1- Hashing', '2- Symmetric encryption', '3- Asymmetric encryption',
+                     '4- Authentication', '5- Quit']
         terminal = TerminalMenu(m_choices)
         entry = terminal.show()
         if entry == 0:
             Encoding.menu()
+            clear()
         elif entry == 1:
             Hash.menu()
+            clear()
+        elif entry == 2:
+            pass
+            clear()
+        elif entry == 3:
+            pass
+            clear()
+        elif entry == 4:
+            pass
+            clear()
         else:
             break
     print('Until next time...')
@@ -19,5 +36,3 @@ def main_menu():
 
 if __name__ == '__main__':
     main_menu()
-
-
