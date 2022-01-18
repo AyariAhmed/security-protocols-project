1116678b1a08beee9cb715e7f11cb9acbf627475
AyariAhmed <ayari17@hotmail.fr>
2022-01-18 20:22:01 +0100

    chore: add insat wl generator

---
+++ utils/gen_insat_dict.py
@@ -0,0 +1,7 @@
+import os
+
+if __name__ == '__main__':
+    # os.system("crunch 26 26 - t @@@@@@.@@@@@@insat.ucar.tn -l aaaaaaaaaaaa@insat.ucar.tn  > insat-dict.txt")
+    # don't run this !
+    pass
+
