1ac2709ceddbdb267b1a9f99f67ae57d50dd87ca
AyariAhmed <ayari17@hotmail.fr>
2022-01-18 03:56:35 +0100

    feat: add mailing service

---
+++ helpers/mailing_service.py
@@ -0,0 +1,13 @@
+import yagmail
+from dotenv import dotenv_values
+
+
+def send_email(receiver_email: str, verification_code: str):
+    config = dotenv_values('.env')
+    email = config['EMAIL']
+    password = config['EMAIL_PASSWORD']
+    print(email,password)
+
+    yag = yagmail.SMTP(email, password)
+
+    yag.send(receiver_email, 'Verification code', verification_code)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from helpers.db_connection import DBConnection
 from auth.authentication import Auth
 from auth.db_queries import find_all_users, find_user_by_email, add_user
-from dotenv import dotenv_values
+from helpers.mailing_service import send_email
 
-print(dotenv_values()['EMAIL'])
-print(dotenv_values()['EMAIL_PASSWORD'])
+
+send_email('ahmed.ayari@insat.ucar.tn', 'heyyyy it really works')
+
+
 
 
 
